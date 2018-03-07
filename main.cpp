de3f72ca4ead09ef5c49c998be83e1063f96c578
Unknown <arashmassoudieh@gmail.com>
2018-03-07 18:48:06 -0500

    API

--- main.cpp
+++ main.cpp
@@ -6,6 +6,12 @@ using namespace std;
 
 int main()
 {
-    CMediumSet M;
+    CMBBlock B("Name=myBlock, area = 10, type=soil, depth=2" );
+    CMedium M;
+    M.showmessages = true;
+    M.set_properties("name=myModel");
+    M.AddBlock(B);
+    M.set_default_params();
+    M.set_properties("dt=0.1");
     M.solve();
 }
