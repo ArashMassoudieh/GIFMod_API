75ce1227bd42b38dc7c4bad334b3e2722b66743e
Arash Massoudieh <arashmassoudieh@gmail.com>
2018-03-07 21:54:01 -0500

    API-3

--- main.cpp
+++ main.cpp
@@ -7,11 +7,12 @@ using namespace std;
 int main()
 {
     CMBBlock B("Name=myBlock, area = 10, type=soil, depth=2" );
-    CMedium M;
+    CMedium M(true);
     M.showmessages = true;
-    M.set_properties("name=myModel");
+    M.set_properties("name=myModel, dt=0.01, tstart=0, tend=1");
     M.AddBlock(B);
     M.set_default_params();
     M.set_properties("dt=0.1");
     M.solve();
+    M.Results.ANS.writetofile("output.txt");
 }
