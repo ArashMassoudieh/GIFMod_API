99364cdc70cbc925d74b3be10203289bde281035
Unknown <arashmassoudieh@gmail.com>
2018-04-23 19:13:14 -0400

    profiling

--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@ int main()
     //VTK_test V;
     //V.TDPoints();
     //V.Point();
-    int nx = 5;
-    int ny = 5;
+    int nx = 10;
+    int ny = 10;
     int nz = 5;
     double dx = 100;
     double dy = 100;
@@ -33,10 +33,10 @@ int main()
                     cout << "Block:" << i << j << k << "has been created" << endl;
                 #endif // Debug_API
                 if (k!=0)
-                    B1 = CMBBlock("name=Soil(" + numbertostring(i) +"."+numbertostring(j) + "." + numbertostring(k) + "), x= " + numbertostring(i*100) + ", y=" + numbertostring(j*100) + ", z=" + numbertostring(k*dz+0.01*dx*i+0.02*dy*j) + ", a= " + numbertostring(dx*dy) + ", type=soil, theta_s=0.4, theta_r=0.1, S=" + numbertostring(dx*dy*dz*(0.4-k/nz*0.2) )+" , vg_n=3, vg_m=0.667, vg_alpha=1, lambda=0.5, z0=" + numbertostring(k*dz+0.01*dx*i+0.02*dy*j) +", V=" + numbertostring(dx*dy*dz) + ", ks=20");
+                    B1 = CMBBlock("name=Soil(" + numbertostring(i) +"."+numbertostring(j) + "." + numbertostring(k) + "), x= " + numbertostring(i*dx) + ", y=" + numbertostring(j*dy) + ", z=" + numbertostring(k*dz+0.01*dx*i+0.02*dy*j) + ", a= " + numbertostring(dx*dy) + ", type=soil, theta_s=0.4, theta_r=0.1, S=" + numbertostring(dx*dy*dz*(0.4-double(k)/double(nz)*0.2) )+" , vg_n=3, vg_m=0.667, vg_alpha=1, lambda=0.5, z0=" + numbertostring(k*dz+0.01*dx*i+0.02*dy*j) +", V=" + numbertostring(dx*dy*dz) + ", ks=20");
 
                 else
-                    B1 = CMBBlock("name=Soil(" + numbertostring(i) +"."+numbertostring(j) + "." + numbertostring(k) + "), x= " + numbertostring(i*100) + ", y=" + numbertostring(j*100) + ", z=" + numbertostring(k*dz+0.01*dx*i+0.02*dy*j) + ",a= " + numbertostring(dx*dy) + ", type=soil, theta_s=0.4, theta_r=0.1, S=" + numbertostring(dx*dy*dz*0.4) + ", vg_n=3, vg_m=0.667, vg_alpha=1, lambda=0.5, z0=" + numbertostring(k*dz+0.01*dx*i+0.02*dy*j) +", V= " + numbertostring(dx*dy*dz) + ", ks=20");
+                    B1 = CMBBlock("name=Soil(" + numbertostring(i) +"."+numbertostring(j) + "." + numbertostring(k) + "), x= " + numbertostring(i*dx) + ", y=" + numbertostring(j*dy) + ", z=" + numbertostring(k*dz+0.01*dx*i+0.02*dy*j) + ",a= " + numbertostring(dx*dy) + ", type=soil, theta_s=0.4, theta_r=0.1, S=" + numbertostring(dx*dy*dz*0.4) + ", vg_n=3, vg_m=0.667, vg_alpha=1, lambda=0.5, z0=" + numbertostring(k*dz+0.01*dx*i+0.02*dy*j) +", V= " + numbertostring(dx*dy*dz) + ", ks=20");
                 #ifdef Debug_API
                     cout << "Block:" << i << j << k << "Assigned!" << endl;
                 #endif // Debug_API
@@ -76,7 +76,7 @@ int main()
     //M.AddBlock(B2);
     //CConnection C("nmanning=0.00001, width=1, name=C, d=1");
     //M.AddConnector("myBlock1","myBlock2",C);
-    M.set_properties("tstart=0, tend=10000, dt=10");
+    M.set_properties("tstart=0, tend=1000, dt=10");
     M.set_default_params();
     M.max_dt() = 100;
     //cout<<M.Connector("C")->tostring();
@@ -86,26 +86,18 @@ int main()
     #ifdef USE_VTK
     VTK_grid gr = M.VTK_get_snap_shot("z0",0,1);
     M.merge_to_snapshot(gr,"ks");
-    M.write_grid_to_vtp(gr,"test_1.vtu");
-    M.write_grid_to_text(gr, "test_1.txt");
+    M.write_grid_to_vtp(gr,"/home/arash/Projects/GIFMOD_API_results/test_1.vtu");
+    M.write_grid_to_text(gr, "/home/arash/Projects/GIFMOD_API_results/test_1.txt");
     #endif // USE_VTK
+    M.solution_method() = "Direct Solution";
     M.solve();
-
-
     #ifdef USE_VTK
-    M.merge_to_snapshot(gr, "theta", 1);
-    M.merge_to_snapshot(gr, "theta", 5);
-    M.merge_to_snapshot(gr, "theta", 25);
-    M.merge_to_snapshot(gr, "theta", 50);
-    M.merge_to_snapshot(gr, "theta", 100);
-    M.merge_to_snapshot(gr, "theta", 200);
-    M.merge_to_snapshot(gr, "theta", 500);
-    M.merge_to_snapshot(gr, "theta", 1000);
-    M.merge_to_snapshot(gr, "theta", 2000);
-    M.merge_to_snapshot(gr, "theta", 5000);
-    M.merge_to_snapshot(gr, "theta", 10000);
-    M.write_grid_to_text(gr, "grid_1.txt");
-    M.write_grid_to_vtp(gr,"test.vtu");
+    for (double t=0; t<1000; t+=100)
+    {
+        VTK_grid grt = M.VTK_get_snap_shot("theta",t,1,"theta");
+        M.write_grid_to_vtp(grt,"/home/arash/Projects/GIFMOD_API_results/test" + numbertostring(t)+ ".vtu");
+    }
+
     #endif // USE_VTK
 
     M.Results.ANS.writetofile(string("text.txt"));
