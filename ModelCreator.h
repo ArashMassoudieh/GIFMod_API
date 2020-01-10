301994ee4a996fcc68f464d3960b77d697c70470
Unknown <arashmassoudieh@gmail.com>
2020-01-10 18:09:35 -0500

    ModelCreator2

--- ModelCreator.h
+++ ModelCreator.h
@@ -4,9 +4,12 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <map>
+#include "MBBlock.h"
 
 using namespace std;
 
+class CMedium;
 class ModelCreator
 {
     public:
@@ -15,12 +18,13 @@ class ModelCreator
         virtual ~ModelCreator();
         ModelCreator(const ModelCreator& other);
         ModelCreator& operator=(const ModelCreator& other);
-
+        bool AddLayer(const string &bodyname, CMedium *M, const string &type, double dx, double dy);
     protected:
 
     private:
         bool file_not_found;
 		bool error;
+		vector<vector<CMBBlock*>> bodies;
 		vector<vector<double>> vals;
 };
 
