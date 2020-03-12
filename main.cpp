c04095619a21761dfc4cc5420514660ae495f0d3
Unknown <arashmassoudieh@gmail.com>
2020-03-12 19:04:57 -0400

    More Complex model

--- main.cpp
+++ main.cpp
@@ -21,21 +21,26 @@ int main()
 	string outputpath = "E:\\Projects\\GIFMod_API_Projects\\outputs\\";
 #else
 	string inputpath = "/home/arash/Projects/GIFMOD_API_results/";
-	string outputpath = "/home/arash/Projects/GIFMOD_API_results/outputs_2/";
+	string outputpath = "/home/arash/Projects/GIFMOD_API_results/outputs/";
 #endif //  Windows
 
 	ModelCreator mCreate;
 
     mCreate.AddBody(string("surface"),string(inputpath + "topo_sligo.csv"),"","nmanning=0.00001");
-    mCreate.AddBody("soil", inputpath + "bedrock.csv", "theta=0.35, porosity=0.4, ks=0.1, theta_s=0.4, theta_r=0.05, vg_alpha=3.6, vg_n=1.56, lambda=0.5, storativity=0.01","");
-	mCreate.getproperties("soil", "depth", inputpath + "depth.csv");
+    mCreate.AddBody("topsoil", inputpath + "topsoilbottom.csv", "theta=0.35, porosity=0.4, ks=0.1, theta_s=0.4, theta_r=0.05, vg_alpha=3.6, vg_n=1.56, lambda=0.5, storativity=0.01","");
+    mCreate.AddBody("deepsoil", inputpath + "bedrock.csv", "theta=0.35, porosity=0.4, ks=0.003, theta_s=0.4, theta_r=0.05, vg_alpha=3.6, vg_n=1.56, lambda=0.5, storativity=0.01","");
+	mCreate.getproperties("topsoil", "depth", inputpath + "topsoildepth.csv");
+	//mCreate.getproperties("deepsoil", "depth", inputpath + "deepsoildepth.csv");
 
     CMedium M(true);
     mCreate.AddLayer("surface",&M,"Catchment",331,508);
-	mCreate.AddLayer("soil", &M, "Soil", 331, 508);
-	mCreate.ConnectBodiesVertical("infiltration",&M, "surface", "soil","",0,0.5);
+	mCreate.AddLayer("topsoil", &M, "Soil", 331, 508);
+	//mCreate.AddLayer("deepsoil", &M, "Soil", 331, 508);
+	mCreate.ConnectBodiesVertical("infiltration",&M, "surface", "topsoil","",0,0.5);
+	//mCreate.ConnectBodiesVertical("percolation",&M, "topsoil", "deepsoil","",0.5,0.5);
     //cout<<"Blocks..."<< endl;
 
+    cout<<"Downstream boundary condition"<<endl;
 	// Creating the downstream boundary condition
 	CMBBlock DownstreamBC("name=DSBC, hs_relationship=15.7315, area=100000, z0=0, type=pond, s=100000");
 	DownstreamBC.set_property("Precipitation","no");
@@ -43,12 +48,14 @@ int main()
 	CConnection DownStreamBC_connect("name=DSBC_c, width = 331, d=250, nmanning=0.00001");
 	CConnection DownStreamBC_connect_ss("name=DSBC_c_soil, area = 10000, d=250, ks=0.5");
 	M.AddConnector("surface(19.1)", "DSBC", DownStreamBC_connect);
-	M.AddConnector("DSBC", "soil(19.1)", DownStreamBC_connect_ss);
+	M.AddConnector("DSBC", "topsoil(19.1)", DownStreamBC_connect_ss);
 	// Creating the downstream boundary condition done!
 
 	M.Precipitation_filename.push_back(inputpath + "rain_real.txt");
     M.set_properties("tstart=40909, tend=41250, dt=0.1");
-
+    cout << M.Block("topsoil(19.1)")->tostring()<<endl;
+    //cout << M.Block("deepsoil(19.1)")->tostring()<<endl;
+    cout<<"Observations"<<endl;
     /* Observation */
     measured_chrc outflow_surface;
     outflow_surface.loc_type = 1;
@@ -81,7 +88,7 @@ int main()
     measured_chrc outflow_head;
     outflow_head.loc_type = 0;
     outflow_head.quan = "h";
-    outflow_head.id.push_back("soil(19.1)");
+    outflow_head.id.push_back("topsoil(19.1)");
     outflow_head.name = "Outflow_Head";
     M.measured_quan().push_back(outflow_head);
 
@@ -93,6 +100,7 @@ int main()
     M.measured_quan().push_back(outflow_surface_head);
     /* Observation */
 
+    cout<<"Solver Settings"<<endl;
     M.check_oscillation() = false;
 	M.outputpathname() = outputpath;
     M.f_load_inflows();
@@ -120,12 +128,15 @@ int main()
     cout << "Creating Outputs" << endl;
     for (double t=40909; t<41250; t+=1)
     {
-        VTK_grid moisture = M.VTK_get_snap_shot("soil", &mCreate, "theta",t,1,"theta");
+        VTK_grid moisture_topsoil = M.VTK_get_snap_shot("topsoil", &mCreate, "theta",t,1,"theta");
+        //VTK_grid moisture_deepsoil = M.VTK_get_snap_shot("deepsoil", &mCreate, "theta",t,1,"theta");
 		VTK_edge_grid Surface_Flow = M.VTK_get_snap_shot_edges("surface", &mCreate, "Q", t, 1, "Q");
 		VTK_edge_grid Subsurface_Flow = M.VTK_get_snap_shot_edges("soil", &mCreate, "Q", t, 1, "Q");
 		VTK_edge_grid Infiltration_Flow = M.VTK_get_snap_shot_edges("infiltration", &mCreate, "Q", t, 1, "Q");
+		//VTK_edge_grid Percolation_Flow = M.VTK_get_snap_shot_edges("percolation", &mCreate, "Q", t, 1, "Q");
         VTK_grid depth = M.VTK_get_snap_shot("surface", &mCreate, "depth",t,1,"depth");
         VTK_grid Infiltration_flow_surf = Infiltration_Flow.toVTKGtid();
+        //VTK_grid Percolation_flow_surf = Percolation_Flow.toVTKGtid();
 
 
         M.write_grid_to_vtp_surf(depth, outputpath + "water_depth" + numbertostring(t) + ".vtp");
@@ -133,9 +144,11 @@ int main()
 		M.write_grid_to_vtp_surf(Surface_Flow, outputpath + "surface_flow" + numbertostring(t) + ".vtp", "Q");
 		M.write_grid_to_vtp_surf(Subsurface_Flow, outputpath + "subsurface_flow" + numbertostring(t) + ".vtp", "Q");
 		M.write_grid_to_vtp_surf(Infiltration_Flow, outputpath + "infiltration_flow" + numbertostring(t) + ".vtp", "Q");
-		M.write_grid_to_vtp_surf(moisture, outputpath + "moisture" + numbertostring(t) + ".vtp");
+		M.write_grid_to_vtp_surf(moisture_topsoil, outputpath + "moisture_topsoil" + numbertostring(t) + ".vtp");
+		//M.write_grid_to_vtp_surf(moisture_deepsoil, outputpath + "moisture_deepsoil" + numbertostring(t) + ".vtp");
 		M.write_grid_to_vtp_surf(Infiltration_flow_surf, outputpath + "infiltration_surf" + numbertostring(t) + ".vtp");
-		M.write_grid_to_text(moisture, outputpath + "moisture" + numbertostring(t) + ".txt");
+		//M.write_grid_to_vtp_surf(Percolation_flow_surf, outputpath + "percolation_surf" + numbertostring(t) + ".vtp");
+		M.write_grid_to_text(moisture_topsoil, outputpath + "moisture_topsoil" + numbertostring(t) + ".txt");
 
     }
 
